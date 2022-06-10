96f098244686449a1c753366d35454dfb8763727
Yan Vallejo <yavallejor@gmail.com>
2022-06-10 14:15:54 -0500

    feat: :sparkles: Add component button

--- components/events/EventItem.jsx
+++ components/events/EventItem.jsx
@@ -1,5 +1,8 @@
-import Link from "next/link";
+import Button from "../ui/Button";
+import DateIcon from "../icons/DateIcon";
 import classes from "./EventItem.module.css";
+import AddressIcon from "../icons/AddresIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
 const EventItem = (props) => {
     const { title, image, location, id, date } = props;
     const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -16,13 +19,20 @@ const EventItem = (props) => {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formattedAddress}</address>
                     </div>
                     <div className={classes.actions}>
-                        <Link href={exploreLink}>Explore Event</Link>
+                        <Button link={exploreLink}>
+                            <span>Explore Event</span>
+                            <span className={classes.icon}>
+                                <ArrowRightIcon />
+                            </span>
+                        </Button>
                     </div>
                 </div>
             </div>
