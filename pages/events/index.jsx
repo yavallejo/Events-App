2a35e431d6d7d9a462a546033578a38e7d90d614
Yan Vallejo <yavallejor@gmail.com>
2022-06-13 17:33:41 -0500

    feat: :sparkles: Add structure filter Events

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,9 +1,11 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventSearch";
 const AllEventPage = () => {
     const events = getAllEvents();
     return (
         <div>
+            <EventSearch />
             <EventList items={events} />
         </div>
     );
