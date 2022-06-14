a3c5793f5141500af9f4fb7f2d175674e94b1f5e
Yan Vallejo <yavallejor@gmail.com>
2022-06-13 22:07:43 -0500

    feat: :sparkles: Add functionallity filter search events

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,5 +1,77 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import EventList from "../../components/events/EventList";
+import ResultsTitle from "../../components/events/resultsTitle";
+
+import { getFilteredEvents } from "../../dummy-data";
+import Button from "../../components/ui/Button";
+import ErrorAlert from "../../components/ui/ErrorAlert";
+
 const ResultsFilterEvents = () => {
-    return <div>ResultsFilterEvents</div>;
+    const router = useRouter();
+
+    const filterData = router.query.slug;
+
+    if (!filterData) {
+        return <p className="center">Loading...</p>;
+    }
+
+    const filteredYears = filterData[0];
+    const filteredMonths = filterData[1];
+
+    const numYear = +filteredYears;
+    const numMonth = +filteredMonths;
+
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return (
+            <Fragment>
+                <ErrorAlert>
+                    <p className="center">
+                        Invalid filter. Please adjust your values!
+                    </p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button link="/events/">Show all events</Button>
+                </div>
+            </Fragment>
+        );
+    }
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <Fragment>
+                <ErrorAlert>
+                    <p className="center">
+                        No events found for the chosen filter!
+                    </p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button link="/events/">Show all events</Button>
+                </div>
+            </Fragment>
+        );
+    }
+
+    const date = new Date(numYear, numMonth);
+
+    return (
+        <Fragment>
+            <ResultsTitle date={date} />
+            <EventList items={filteredEvents} />
+        </Fragment>
+    );
 };
 
 export default ResultsFilterEvents;
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,23 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventSearch from "../../components/events/EventSearch";
 const AllEventPage = () => {
     const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath);
+    }
+
     return (
-        <div>
-            <EventSearch />
+        <Fragment>
+            <EventSearch onSearch={findEventHandler} />
             <EventList items={events} />
-        </div>
+        </Fragment>
     );
 };
 
